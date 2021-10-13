3c85397f5f543807ab0a2501da00c359280f6f13
luke7102 <cruel8498@naver.com>
2021-10-14 08:56:41 +0900

    Javascript: Scrolling Element

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
-'use strict';
+'use strict'
 
 // Make navbar transparent when it is on the top.
-const navbar = document.querySelector('#navbar');
-const navbarHeight = navbar.getBoundingClientRect().height;
+const navbar = document.querySelector('#navbar')
+const navbarHeight = navbar.getBoundingClientRect().height
 document.addEventListener('scroll', () => {
    if(window.scrollY > navbarHeight) {
-       navbar.classList.add('navbar--dark');
+       navbar.classList.add('navbar--dark')
    } else {
-       navbar.classList.remove('navbar--dark');
+       navbar.classList.remove('navbar--dark')
    }
-});
+})
 
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu')
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target
+    const link = target.dataset.link
+    if(link == null) {
+        return
+    }
+    scrollIntoView(link)
+})
+
+// Handle click on "Contact Me"
+const contactBtn = document.querySelector('.home__contact')
+contactBtn.addEventListener('click', () => {
+    scrollIntoView('#contact')
+})
+
+function scrollIntoView(selector) {
+    const selectedElement = document.querySelector(selector)
+    selectedElement.scrollIntoView({behavior:'smooth'})
+}
