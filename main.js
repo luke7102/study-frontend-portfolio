93517c0214c4928de1259eb045cb3965679ffd53
luke7102 <cruel8498@naver.com>
2021-10-14 18:04:41 +0900

    End Project

--- main.js
+++ main.js
@@ -4,11 +4,11 @@
 const navbar = document.querySelector('#navbar')
 const navbarHeight = navbar.getBoundingClientRect().height
 document.addEventListener('scroll', () => {
-   if(window.scrollY > navbarHeight) {
-       navbar.classList.add('navbar--dark')
-   } else {
-       navbar.classList.remove('navbar--dark')
-   }
+    if (window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar--dark')
+    } else {
+        navbar.classList.remove('navbar--dark')
+    }
 })
 
 // Handle scrolling when tapping on the navbar menu
@@ -16,7 +16,7 @@ const navbarMenu = document.querySelector('.navbar__menu')
 navbarMenu.addEventListener('click', (event) => {
     const target = event.target
     const link = target.dataset.link
-    if(link == null) {
+    if (link == null) {
         return
     }
     navbarMenu.classList.remove('open')
@@ -45,7 +45,7 @@ document.addEventListener('scroll', () => {
 // Show "arrow up" button when scrolling down
 const arrowUp = document.querySelector('.arrow-up')
 document.addEventListener('scroll', () => {
-    if(window.scrollY >= homeContainerHeight / 2) {
+    if (window.scrollY >= homeContainerHeight / 2) {
         arrowUp.classList.add('visible')
     } else {
         arrowUp.classList.remove('visible')
@@ -72,7 +72,7 @@ workBtnContainer.addEventListener('click', (e) => {
     projectContainer.classList.add('anim-out')
     setTimeout(() => {
         projects.forEach(project => {
-            if(filter === '*' || filter === project.dataset.type) {
+            if (filter === '*' || filter === project.dataset.type) {
                 project.classList.remove('invisible')
             } else {
                 project.classList.add('invisible')
@@ -82,7 +82,63 @@ workBtnContainer.addEventListener('click', (e) => {
     }, 300)
 })
 
+// 1. 모든 섹션 요소들을 가지고 온다.
+// 2. IntersactionObserver를 이용해서 모든 섹션들을 관찰한다.
+// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다.
+
+const sectionIds = [
+    '#home',
+    '#about',
+    '#skills',
+    '#work',
+    '#testimonials',
+    '#contact'
+]
+const sections = sectionIds.map(id => document.querySelector(id))
+const navItems = sectionIds.map(id => document.querySelector(`[data-link="${id}"]`))
+
+let selectedNavIndex = 0
+let selectedNavItem = navItems[0]
+
+function selectNavItem(selected) {
+    selectedNavItem.classList.remove('active')
+    selectedNavItem = selected
+    selectedNavItem.classList.add('active')
+}
+
 function scrollIntoView(selector) {
     const selectedElement = document.querySelector(selector)
-    selectedElement.scrollIntoView({behavior:'smooth'})
+    selectedElement.scrollIntoView({behavior: 'smooth'})
+    selectNavItem(navItems[sectionIds.indexOf(selector)])
 }
+
+const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.3
+}
+
+const observerCallback = (entries, observer) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+            const index = sectionIds.indexOf(`#${entry.target.id}`)
+            // 스크롤링이 아래로 되어서 페이지가 올라옴
+            if (entry.boundingClientRect.y < 0) {
+                selectedNavIndex = index + 1
+            } else {
+                selectedNavIndex = index - 1
+            }
+        }
+    })
+}
+const observer = new IntersectionObserver(observerCallback, observerOptions)
+sections.forEach(section => observer.observe(section))
+
+window.addEventListener('wheel', () => {
+    if (window.scrollY === 0) {
+        selectedNavIndex = 0
+    } else if (Math.round(window.scrollY + window.innerHeight) >= document.body.clientHeight) {
+        selectedNavIndex = navItems.length - 1
+    }
+    selectNavItem(navItems[selectedNavIndex])
+})
