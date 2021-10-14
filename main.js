ec2e32c58ba8c7969280a2c7cda6a1bd5e78042b
luke7102 <cruel8498@naver.com>
2021-10-14 13:24:17 +0900

    Javascript: Work Button State

--- main.js
+++ main.js
@@ -56,6 +56,12 @@ const projectContainer = document.querySelector('.work__projects')
 const projects = document.querySelectorAll('.project')
 workBtnContainer.addEventListener('click', (e) => {
     const filter = e.target.dataset.filter
+
+    // Remove selection from the previous item and select the new one
+    const active = document.querySelector('.category__btn.selected')
+    active.classList.remove('selected')
+    e.target.classList.add('selected')
+
     projectContainer.classList.add('anim-out')
     setTimeout(() => {
         projects.forEach(project => {
